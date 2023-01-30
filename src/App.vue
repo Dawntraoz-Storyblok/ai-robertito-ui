98096a593a9e4c8f9ca7fc080138fbda8eb1f197
spences10 <spences10apps@gmail.com>
2023-01-30 14:29:52 +0000

    feat: :sparkles: add header component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <script setup lang="ts">
-  import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from "vue-router";
+import TheHeader from "./components/TheHeader.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Storyblok Logo" class="logo" src="@/assets/logo.svg" width="158" height="33" />
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <TheHeader />
 
   <RouterView />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
