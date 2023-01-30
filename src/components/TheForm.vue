3609f3977d932fa58b4eeb09331aa3a41ee341fe
Dawntraoz <albasilventefu@gmail.com>
2023-01-30 17:20:16 +0100

    fix: ts build

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="container mx-auto max-w-4xl" @submit.prevent="aiQuestion">
+  <form type="submit" class="container mx-auto max-w-4xl" @submit.prevent="aiQuestion">
     <div class="grid grid-cols-1 md:grid-cols-2 items-center gap-8 max-w-4xl mx-auto py-6 md:py-10">
       <img src="../assets/ai_robertito.png" class="mx-auto w-64 md:w-auto max-w-full rounded-full shadow-2xl" />
       <div class="text-center md:text-start">
@@ -7,9 +7,8 @@
         <label for="question" class="block py-3">
           <span class="text-md">Ask your question to the master.</span>
         </label>
-        <input id="question" type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs"
-          v-model="question" />
-        <button class="btn btn-ghost btn-circle -translate-x-12 translate-y-1">
+        <input id="question" name="question" type="search" placeholder="Type here" class="input input-bordered w-full max-w-xs" autocomplete="on" v-model="question" />
+        <button type="submit" class="btn btn-ghost btn-circle -translate-x-12 translate-y-1">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
               d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -18,7 +17,7 @@
       </div>
     </div>
   </form>
-  <Markdown v-if="results" :source="results" class="w-full max-w-4xl mx-auto rounded-xl bg-primary-focus text-white p-8 prose" />
+  <Markdown v-if="results" :source="results" class="w-full max-w-4xl mx-auto rounded-xl bg-primary-focus text-white p-8 prose prose-xl" />
 </template>
 
 <script setup>
