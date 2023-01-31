1c3308b9752be6c01f5d54b807db79784a967613
Roberto Butti <roberto.butti@gmail.com>
2023-01-31 16:12:21 +0100

    add NLP

--- netlify/functions/question.js
+++ netlify/functions/question.js
@@ -1,5 +1,51 @@
+const { containerBootstrap } = require('@nlpjs/core');
+const { Nlp } = require('@nlpjs/nlp');
+const { LangEn } = require('@nlpjs/lang-en');
+const { dockStart, ConsoleConnector } = require('@nlpjs/basic');
+const path = require("path");
+
+const fs = require('fs');
+
 exports.handler = async function (event, context) {
     // your server-side functionality
+    let dock;
+
+    const localDir = path.resolve(__dirname);
+//    (async () => {
+        dock = await dockStart({ use: ['Basic', 'Qna'], nlu: { useNoneFeature: false } });
+        nlp = await dock.get('nlp');
+        if (fs.existsSync(localDir + '/model.nlp')) {
+            await nlp.load(localDir +'/model.nlp');
+            console.log(nlp)
+        } else {
+            //return;
+        }
+ //   })();
+    question = event.queryStringParameters.question
+
+    console.log(question)
+    const response = await nlp.process('en', question);
+    console.log(response);
+    let answer = ""
+    if (response.answer) {
+        answer = response.answer.trim()
+    } else {
+        console.log("I don't know the answer, help me to train")
+        answer = ""
+    }
+
+    let data = "Sorry I don't know the answer for your question: " + question
+    try {
+
+        answer = answer.replace(".", "/")
+        data = fs.readFileSync(localDir + '/content/' + answer + '.md', 'utf8');
+        console.log('💫💫💫💫💫💫💫💫💫💫💫💫💫')
+        console.log(data);
+        console.log('💫💫💫💫💫💫💫💫💫💫💫💫💫')
+    } catch (err) {
+        //data = "Error:" + err
+        console.error(err);
+    }
     return {
         statusCode: 200,
         headers: {
@@ -11,6 +57,11 @@ exports.handler = async function (event, context) {
             'Access-Control-Allow-Headers': 'Content-Type',
             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
         },
-        body: JSON.stringify({ answer: "I will answer you later." }),
+        body: JSON.stringify(
+            { answer: data,
+                question: question,
+                answerCode: answer,
+                response: response
+             }),
     };
 };
